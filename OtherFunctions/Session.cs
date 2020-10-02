a7d6c34043b1cc2a75163aac78714a57f95de008
Luuk Kenselaar <luuk2014@hotmail.com>
2020-10-02 12:11:53 +0200

    Created static class that stores session details

---
+++ OtherFunctions/Session.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherFunctions
+{
+    public static class Session
+    {
+        public static int userID { get; set; }
+        public static string name { get; set; }
+
+        /*
+         * Wipe the current session
+         */
+        public static void Wipe()
+        {
+            userID = 0;
+            name = null;
+        }
+    }
+}
