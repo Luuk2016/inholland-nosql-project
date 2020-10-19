7c0db4d4fd7d791dad4652f701b01a047375d7d8
Luuk Kenselaar <luuk2014@hotmail.com>
2020-10-19 19:20:47 +0200

    WIP: First part of the statistics

--- UI/Panels/DashboardStatistics.Designer.cs
+++ UI/Panels/DashboardStatistics.Designer.cs
@@ -29,21 +29,19 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 3D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 5D);
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 3D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 5D);
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.chPastDeadline = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.lblTitle = new System.Windows.Forms.Label();
             this.btnShowList = new System.Windows.Forms.Button();
-            this.chUnresolved = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chPastDeadline = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chResolvedUnresolvedTickets = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chUnresolved)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chPastDeadline)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chResolvedUnresolvedTickets)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -51,18 +49,36 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.chPastDeadline, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.lblTitle, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnShowList, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.chUnresolved, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.chPastDeadline, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.chResolvedUnresolvedTickets, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.01676F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.98324F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(962, 716);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // chPastDeadline
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chPastDeadline.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chPastDeadline.Legends.Add(legend1);
+            this.chPastDeadline.Location = new System.Drawing.Point(484, 132);
+            this.chPastDeadline.Name = "chPastDeadline";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series1.Legend = "Legend1";
+            series1.Name = "pastDeadline";
+            this.chPastDeadline.Series.Add(series1);
+            this.chPastDeadline.Size = new System.Drawing.Size(475, 390);
+            this.chPastDeadline.TabIndex = 5;
+            this.chPastDeadline.Text = "Past deadline/On time tickets";
             // 
             // lblTitle
             // 
@@ -90,46 +106,23 @@
             this.btnShowList.UseVisualStyleBackColor = false;
             this.btnShowList.Click += new System.EventHandler(this.btnShowList_Click);
             // 
-            // chUnresolved
+            // chResolvedUnresolvedTickets
             // 
-            this.chUnresolved.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.chUnresolved.BorderlineColor = System.Drawing.Color.Gold;
-            this.chUnresolved.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
-            this.chUnresolved.BorderlineWidth = 2;
-            chartArea1.Name = "ChartArea1";
-            this.chUnresolved.ChartAreas.Add(chartArea1);
-            this.chUnresolved.Location = new System.Drawing.Point(3, 132);
-            this.chUnresolved.Name = "chUnresolved";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series1.Name = "UnresolvedTickets";
-            dataPoint1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            dataPoint2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            series1.Points.Add(dataPoint1);
-            series1.Points.Add(dataPoint2);
-            this.chUnresolved.Series.Add(series1);
-            this.chUnresolved.Size = new System.Drawing.Size(475, 390);
-            this.chUnresolved.TabIndex = 2;
-            // 
-            // chPastDeadline
-            // 
-            this.chPastDeadline.BorderlineColor = System.Drawing.Color.Crimson;
-            this.chPastDeadline.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
-            this.chPastDeadline.BorderlineWidth = 2;
             chartArea2.Name = "ChartArea1";
-            this.chPastDeadline.ChartAreas.Add(chartArea2);
-            this.chPastDeadline.Location = new System.Drawing.Point(484, 132);
-            this.chPastDeadline.Name = "chPastDeadline";
+            this.chResolvedUnresolvedTickets.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chResolvedUnresolvedTickets.Legends.Add(legend2);
+            this.chResolvedUnresolvedTickets.Location = new System.Drawing.Point(3, 132);
+            this.chResolvedUnresolvedTickets.Name = "chResolvedUnresolvedTickets";
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series2.Name = "TicketsPastDeadline";
-            dataPoint3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            dataPoint4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            series2.Points.Add(dataPoint3);
-            series2.Points.Add(dataPoint4);
-            this.chPastDeadline.Series.Add(series2);
-            this.chPastDeadline.Size = new System.Drawing.Size(475, 390);
-            this.chPastDeadline.TabIndex = 3;
+            series2.Legend = "Legend1";
+            series2.Name = "unresolved-resolved-tickets";
+            this.chResolvedUnresolvedTickets.Series.Add(series2);
+            this.chResolvedUnresolvedTickets.Size = new System.Drawing.Size(475, 390);
+            this.chResolvedUnresolvedTickets.TabIndex = 4;
+            this.chResolvedUnresolvedTickets.Tag = "";
+            this.chResolvedUnresolvedTickets.Text = "Unresolved/Resolved tickets";
             // 
             // DashboardStatistics
             // 
@@ -142,8 +135,8 @@
             this.Text = "DashboardStatistics";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chUnresolved)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chPastDeadline)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chResolvedUnresolvedTickets)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -153,7 +146,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Button btnShowList;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chUnresolved;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chResolvedUnresolvedTickets;
         private System.Windows.Forms.DataVisualization.Charting.Chart chPastDeadline;
     }
 }
